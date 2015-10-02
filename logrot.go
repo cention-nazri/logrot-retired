3331e7754d28ec914ea75d753f4dcde00bfc44e6
Nazri Ramliy <ayiehere@gmail.com>
2015-10-02 13:03:55 +0800

    Add test

--- logrot.go
+++ logrot.go
@@ -21,11 +21,20 @@ type LogRot struct {
 	name    string
 	LogFile *os.File
 	signal  os.Signal
+	quit    chan struct{}
 }
 
 // WriteTo sets the log output to the given file and reopen the file on SIGHUP.
 func WriteTo(name string) *LogRot {
 	return rotateOn(name, syscall.SIGHUP)
+}
+
+// WriteAllTo sets the log output, os.Stdout and os.Stderr to the given file and reopen the file on SIGHUP.
+func WriteAllTo(name string) *LogRot {
+	lr := WriteTo(name)
+	os.Stdout = lr.LogFile
+	os.Stderr = lr.LogFile
+	return lr
 }
 
 // rotateOn rotates the log file on the given signals
@@ -34,20 +43,22 @@ func rotateOn(name string, sig os.Signal) *LogRot {
 		name:    name,
 		signal:  sig,
 		LogFile: mustOpenFileForAppend(name),
+		quit:    make(chan struct{}),
 	}
 	log.SetOutput(rl.LogFile)
 
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, sig)
 	go func() {
-		for s := range sigs {
-			switch s {
-			case rl.signal:
-				log.Printf("%s received - rotating log file handle on %s\n", s, rl.name)
-				oldLog := rl.LogFile
-				rl.LogFile = mustOpenFileForAppend(rl.name)
-				log.SetOutput(rl.LogFile)
-				oldLog.Close()
+		for {
+			select {
+			case s := <-sigs:
+				if s == rl.signal {
+					log.Printf("%s received - rotating log file handle on %s\n", s, rl.name)
+					rl.rotate()
+				}
+			case <-rl.quit:
+				return
 			}
 		}
 	}()
@@ -56,6 +67,14 @@ func rotateOn(name string, sig os.Signal) *LogRot {
 
 func (rl *LogRot) Close() {
 	if rl != nil && rl.LogFile != nil {
+		rl.quit <- struct{}{}
 		rl.LogFile.Close()
 	}
+}
+
+func (rl *LogRot) rotate() {
+	oldLog := rl.LogFile
+	rl.LogFile = mustOpenFileForAppend(rl.name)
+	log.SetOutput(rl.LogFile)
+	oldLog.Close()
 }
